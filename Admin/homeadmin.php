161bcae8b6b09a1ea6947454ed4e0158160092e4
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 09:38:49 +0700

    Create homeadmin.php

---
+++ Admin/homeadmin.php
@@ -0,0 +1,61 @@
+<div class="page-header">
+  <h3 class="page-title">
+    <span class="page-title-icon bg-gradient-primary text-white mr-2">
+      <i class="mdi mdi-home"></i>
+    </span> 
+    Dashboard 
+  </h3>
+</div>
+
+<div class="row">
+  <div class="col-12 grid-margin">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Halo Admin!!</h4>
+        <div class="table-responsive">
+          Semua akan indah di masanya, jika sekarang masih menemui kesulitan berarti tandanya sebuah perjalanan belum berakhir.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  
+  <div class="col-md-4 stretch-card grid-margin">
+    <div class="card bg-gradient-danger card-img-holder text-white">
+      <div class="card-body">
+        <img src="Web/assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>
+        <h4 class="font-weight-normal mb-3">
+          Grup Soal
+          <i class="mdi mdi-chart-line mdi-24px float-right"></i>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-md-4 stretch-card grid-margin">
+    <div class="card bg-gradient-info card-img-holder text-white">
+      <div class="card-body">
+        <img src="Web/assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>
+        <h4 class="font-weight-normal mb-3">
+          Siswa
+          <i class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
+        </h4>
+      </div>
+    </div>
+  </div>
+  
+  <div class="col-md-4 stretch-card grid-margin">
+    <div class="card bg-gradient-success card-img-holder text-white">
+      <div class="card-body">
+        <img src="Web/assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image"/>
+        <h4 class="font-weight-normal mb-3">
+          Ujian
+          <i class="mdi mdi-diamond mdi-24px float-right"></i>
+        </h4>
+      </div>
+    </div>
+  </div>
+
+</div>
