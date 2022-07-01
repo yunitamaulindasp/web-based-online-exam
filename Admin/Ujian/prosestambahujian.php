b886a851d103dc2e8e1a16c750b5767016d77c2c
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 10:52:38 +0700

    Create prosestambahujian.php

---
+++ Admin/Ujian/prosestambahujian.php
@@ -0,0 +1,24 @@
+<?php
+	$nama = $_POST['nama'];
+	$grup = $_POST['grup'];
+	$token = $_POST['token'];
+	$waktu = $_POST['waktu'];
+	$banyak = $_POST['banyaksoal'];
+	
+	require("koneksi.php");
+	$query = "INSERT INTO setujian (Nama, Grupsoal, Token, Waktu, Banyaksoal) VALUES ('$nama', '$grup', '$token', '$waktu', '$banyak')";
+	$hasil = $mysqli->query($query);	
+	
+	if (!$hasil)
+	{	echo "Eror: " . $mysqli->error;
+	}
+	else
+	{
+		echo "<table width='100%' border='0'>";
+		echo "<tr><td>Ujian:</td><td>$nama</td></tr>";
+		echo "<tr><td>Token:</td><td>$token</td></tr>";
+		echo "<tr><td>Waktu Pengerjaan Ujian:</td><td>$waktu menit</td></tr>";
+		echo "</table>";
+	}
+	$mysqli->close();
+?>
