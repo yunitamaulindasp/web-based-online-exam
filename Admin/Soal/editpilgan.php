d6e4b57a79833266491b63bcde8d1ddec04dadb0
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 12:08:24 +0700

    Update editpilgan.php

--- Admin/Soal/editpilgan.php
+++ Admin/Soal/editpilgan.php
@@ -17,7 +17,7 @@
 ?>
 		<input type="hidden" name="kode" id="kode" value="<?php echo $soal; ?>">
 		<div class="col-12 grid-margin stretch-card">
-      <div class="card">
+			<div class="card">
 				<div class="card-body">
 					 <h4 class="card-title">Soal</h4>
 					<textarea id="soal" name="soal" class="ckeditor">
@@ -26,8 +26,8 @@
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
-      <div class="card">
+		<div class="col-12 grid-margin stretch-card">
+			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan a</h4>
 					<textarea id="pilihana" name="pilihana"  class="ckeditor">
@@ -36,7 +36,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan b</h4>
@@ -46,7 +46,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan c</h4>
@@ -56,7 +56,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan d</h4>
@@ -66,7 +66,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="form-group">
+		<div class="form-group">
 			<h3 class="page-title"> Pilihan Benar </h3>
 			<select class="form-control form-control" name="jawaban" id="jawaban">
 				<option <?php if($data['jawaban']=='a'){echo "selected"; } ?> value="a">Pilihan A</option>
@@ -75,7 +75,7 @@
 				<option <?php if($data['jawaban']=='d'){echo "selected"; } ?> value="d">Pilihan D</option>
 			</select>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pembahasan</h4>
@@ -85,10 +85,10 @@
 				</div>
 			</div>
 		</div>
-    <button type="submit" class="btn btn-gradient-info btn-icon-text col-12">
+		<button type="submit" class="btn btn-gradient-info btn-icon-text col-12">
 			<i class="mdi mdi-file-check btn-icon-prepend"></i>
 			Edit Soal
 		</button>
-    </form>
+	</form>
 </div>
 <script src="Web/js/helpers/ckeditor/ckeditor.js"></script>
