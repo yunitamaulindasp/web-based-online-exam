f222cc65f914f3e19a6222c196e544e235e97fe1
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 10:44:25 +0700

    Create proseseditbensal.php

---
+++ Admin/Soal/proseseditbensal.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+	$idsoal = $_POST['kode'];
+	$soal = $_POST['soal'];
+	$jawab = $_POST['jawaban'];
+	$pembahasan = $_POST['pembahasan'];
+	
+	require("koneksi.php");
+	$query = "UPDATE soal SET 
+            soal='$soal',
+            jawaban='$jawab',
+            pembahasan='$pembahasan'
+            WHERE kodesoal='$idsoal' ";
+	$hasil = $mysqli->query($query);
+	if (!$hasil)
+	{	echo "Eror: " . $mysqli->error;
+	}
+	else
+	{	echo "<script>alert('proses edit berhasil');</script>";
+        header('location:../../?kode=tambahsoal&grupsoal='.$_SESSION['idgrup']);
+	}
+	$mysqli->close();
+?>
