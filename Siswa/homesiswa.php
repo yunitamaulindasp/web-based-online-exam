af474cb442018f01b720486177615cebadc08e47
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 12:32:48 +0700

    Create homesiswa.php

---
+++ Siswa/homesiswa.php
@@ -0,0 +1,21 @@
+<div class="page-header">
+	<h3 class="page-title">
+		<span class="page-title-icon bg-gradient-primary text-white mr-2">
+			<i class="mdi mdi-home"></i>
+		</span> 
+		Dashboard 
+	</h3>
+</div>
+
+<div class="row">
+	<div class="col-12 grid-margin">
+		<div class="card">
+			<div class="card-body">
+				<h4 class="card-title">Halo Siswa!!</h4>
+				<div class="table-responsive">
+					"Jika kamu jatuh kemarin, berdiri hari ini." - H.G. Wells
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
