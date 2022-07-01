e4a5cd8d34c51e0bfe242b347a0bca399449a68e
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 11:37:20 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,11 @@
 <?php
 	session_start();
-	if (!isset($_SESSION['nama'])) //bila tidak ada var. session
+	if (!isset($_SESSION['nama']))
 	{	echo "<script>alert('Anda belum login, silakan login lebih dulu')</script>";
 		echo "<script>window.location.replace('login.php')</script>";
 		die();
 	}
-	session_destroy(); //hentikan session
+	session_destroy();
 	echo "<script>alert('Terimakasih')</script>";
 	echo "<script>window.location.replace('login.php')</script>";
 ?>
