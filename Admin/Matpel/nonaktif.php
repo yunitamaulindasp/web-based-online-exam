a57a45da165ea72d224a85d2476e7011b75f35e4
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 11:52:57 +0700

    Update nonaktif.php

--- Admin/Matpel/nonaktif.php
+++ Admin/Matpel/nonaktif.php
@@ -1,12 +1,12 @@
 <?php
 	$kode = $_GET['matpel'];
 	require 'koneksi.php';
-  $sql = "SELECT * FROM matapelajaran WHERE kodematpel='$kode'";
+	$sql = "SELECT * FROM matapelajaran WHERE kodematpel='$kode'";
 	$hasil = $mysqli->query($sql) or die("Error: " . $mysqli->error);
 	if ($hasil->num_rows == 0)
 	{	die("Mata pelajaran tidak ditemukan!");
 	}
-  $sql = "UPDATE matapelajaran SET aktif='T' WHERE kodematpel='$kode' ";
+  	$sql = "UPDATE matapelajaran SET aktif='T' WHERE kodematpel='$kode' ";
 	$hasil = $mysqli->query($sql) or die ("Error: ". $mysqli->error);
 	if ($mysqli->affected_rows > 0)
 	{	echo "<script> alert('Mata pelajaran berhasil dinonaktifkan.');
