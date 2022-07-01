1269e3d6312e11e894ad160347ac0af9eb602936
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 12:12:26 +0700

    Update pilgan.php

--- Admin/Soal/pilgan.php
+++ Admin/Soal/pilgan.php
@@ -9,23 +9,23 @@
 
 <div class="row">
 	<form class="forms-sample" name="tambah" id="tambah" action="admin/soal/prosespilgan.php" method="POST">
-    <div class="col-12 grid-margin stretch-card">
-      <div class="card">
+		<div class="col-12 grid-margin stretch-card">
+			<div class="card">
 				<div class="card-body">
 					 <h4 class="card-title">Soal</h4>
 					<textarea id="soal" name="soal" class="ckeditor"></textarea>
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
-      <div class="card">
+		<div class="col-12 grid-margin stretch-card">
+			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan a</h4>
 					<textarea id="pilihana" name="pilihana"  class="ckeditor"></textarea>
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan b</h4>
@@ -33,7 +33,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan c</h4>
@@ -41,7 +41,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pilihan d</h4>
@@ -49,7 +49,7 @@
 				</div>
 			</div>
 		</div>
-    <div class="form-group">
+		<div class="form-group">
 			<h3 class="page-title"> Pilihan Benar </h3>
 			<select class="form-control form-control" name="jawaban" id="jawaban">
 				<option value="a">Pilihan A</option>
@@ -58,7 +58,7 @@
 				<option value="d">Pilihan D</option>
 			</select>
 		</div>
-    <div class="col-12 grid-margin stretch-card">
+		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
 					<h4 class="card-title">Pembahasan</h4>
@@ -66,10 +66,10 @@
 				</div>
 			</div>
 		</div>
-    <button type="submit" class="btn btn-gradient-info btn-icon-text col-12">
+		<button type="submit" class="btn btn-gradient-info btn-icon-text col-12">
 			<i class="mdi mdi-file-check btn-icon-prepend"></i>
 			Tambah Soal
 		</button>
-    </form>
+	</form>
 </div>
 <script src="Web/js/helpers/ckeditor/ckeditor.js"></script>
