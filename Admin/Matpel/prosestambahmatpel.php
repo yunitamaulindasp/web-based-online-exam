bcd7fd1db2b6c8ce2052fb1a4e99eafd2f9ab588
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 09:48:25 +0700

    Create prosestambahmatpel.php

---
+++ Admin/Matpel/prosestambahmatpel.php
@@ -0,0 +1,17 @@
+<?php
+	$nama = $_POST['nama'];
+	$tahun = $_POST['tahun'];
+	
+	$query = "INSERT INTO matapelajaran (matpel, tahunajaran, aktif) VALUES ('$nama', '$tahun', 'Y')";
+	$hasil = $mysqli->query($query);
+	if (!$hasil)
+	{	echo "Eror: " . $mysqli->error;
+	}
+	else
+	{	echo "<table width='100%' border='0'>";
+		echo "<tr><td>Mata Pelajaran:</td><td>$nama</td></tr>";
+		echo "<tr><td>Tahun Ajaran:</td><td>$tahun</td></tr>";
+		echo "</table>";
+	}
+	$mysqli->close();
+?>
