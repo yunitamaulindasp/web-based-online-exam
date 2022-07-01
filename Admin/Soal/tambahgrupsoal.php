ca1b7e5880a704079e6f3503e77e0f88ea44a4d5
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 12:17:17 +0700

    Update tambahgrupsoal.php

--- Admin/Soal/tambahgrupsoal.php
+++ Admin/Soal/tambahgrupsoal.php
@@ -12,34 +12,34 @@
 		<div class="col-12 grid-margin stretch-card">
 			<div class="card">
 				<div class="card-body">
-          <h4 class="card-title"> Tambah Grup Soal </h4>
-          <div class="form-group row">
+          				<h4 class="card-title"> Tambah Grup Soal </h4>
+          				<div class="form-group row">
 						<label class="col-sm-2 col-form-label">Nama Grup Soal</label>
 						<div class="col-sm-10">
 							<input type="text" class="form-control" name="nama" id="nama" placeholder="Nama Grup Soal">
 						</div>
 					</div>
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label">Mata Pelajaran</label>
-            <div class="col-sm-10">
-              <select class="form-control" name="pelajaran" id="pelajaran">
+          				<div class="form-group row">
+            					<label class="col-sm-2 col-form-label">Mata Pelajaran</label>
+            					<div class="col-sm-10">
+							<select class="form-control" name="pelajaran" id="pelajaran">
 								<?php
 									$sql= "SELECT kodematpel, matpel FROM matapelajaran WHERE aktif='Y'";
 									$hasil= $mysqli->query($sql) or die("Error: ". $mysli->error);
 									While ($arData = $hasil->fetch_array())
 									{
 								?>
-                <option value="<?php echo $arData['kodematpel']; ?>"><?php echo '<td>'.$arData['matpel']. '</td>'; ?></option>
+								<option value="<?php echo $arData['kodematpel']; ?>"><?php echo '<td>'.$arData['matpel']. '</td>'; ?></option>
 								<?php 
 									}
 								?>
-              </select>
-            </div>
-          </div>
-        </div>
+							</select>
+            					</div>
+          				</div>
+        			</div>
 			</div>
 		</div>
-    <button type="submit" class="btn btn-danger col-12">Tambah Grup Soal</button>
+    		<button type="submit" class="btn btn-danger col-12">Tambah Grup Soal</button>
 	</div>	
 </form>
 
