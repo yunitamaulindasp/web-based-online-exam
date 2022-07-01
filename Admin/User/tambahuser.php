8a37a532c9656ca97dc512043c770584d2a2b2db
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 12:29:46 +0700

    Update tambahuser.php

--- Admin/User/tambahuser.php
+++ Admin/User/tambahuser.php
@@ -1,92 +1,93 @@
 <div class="col-12 grid-margin stretch-card">
-  <div class="card">
-    <div class="card-body">
-      <form class="forms-sample" name="formisi" id="formisi" >
-        <h4 class="card-title"> Tambah User </h4>
-        <p class="card-description"> Profil </p>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Nama</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" name="nama" id="nama" placeholder="Name">
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Jenis Kelamin</label>
-          <div class="col-md-5">
-            <div class="form-group">
-              <div class="form-check-primary">
-                <label class="form-check-label">
-                  <input type="radio" class="form-check-input" name="gender" id="gender1" value="L" checked>
-                  Laki-laki
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-5">
-            <div class="form-group">
-              <div class="form-check-primary">
-                <label class="form-check-label">
-                  <input type="radio" class="form-check-input" name="gender" id="gender2" value="P">
-                  Perempuan
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Tempat Lahir</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" name="tempat" id="tempat" placeholder="Tempat Lahir">
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Tanggal Lahir</label>
-          <div class="col-sm-5">
-            <input type="date" class="form-control" name="tgl" id="tgl" placeholder="dd/mm/yyyy"/>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Status</label>
-          <div class="col-sm-10">
-            <select class="form-control" name="status" id="status">
-              <option value="admin">Admin</option>
-              <option value="siswa">Siswa</option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">File upload</label>
-          <input type="file" name="foto" id="foto" class="file-upload-default">
-          <div class="input-group col-sm-10">
-            <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
-            <span class="input-group-append">
-              <button class="file-upload-browse btn btn-gradient-primary" type="button">Upload</button>
-            </span>
-          </div>
-        </div>
-        <p class="card-description"> Akun </p>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Username</label>
-          <div class="col-sm-10">
-            <div class="input-group mb-2 mr-sm-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">@</div>
-              </div>
-              <input type="text" class="form-control" name="username" id="username" placeholder="Username">
-            </div>
-          </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-sm-2 col-form-label">Password</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control mb-2 mr-sm-2" name="password" id="password" placeholder="Password">
-          </div>
-        </div> 
-        <button type="submit" class="btn btn-gradient-primary mr-2">Submit</button>
-      </form>
-    </div>
-  </div>
+  	<div class="card">
+    		<div class="card-body">
+      			<form class="forms-sample" name="formisi" id="formisi" >
+        			<h4 class="card-title"> Tambah User </h4>
+        			<p class="card-description"> Profil </p>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Nama</label>
+          				<div class="col-sm-10">
+            					<input type="text" class="form-control" name="nama" id="nama" placeholder="Name">
+          				</div>
+        			</div>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Jenis Kelamin</label>
+          				<div class="col-md-5">
+            					<div class="form-group">
+              						<div class="form-check-primary">
+                						<label class="form-check-label">
+                  							<input type="radio" class="form-check-input" name="gender" id="gender1" value="L" checked>
+                  							Laki-laki
+                						</label>
+              						</div>
+            					</div>
+          				</div>
+          				<div class="col-md-5">
+            					<div class="form-group">
+              						<div class="form-check-primary">
+                						<label class="form-check-label">
+                  							<input type="radio" class="form-check-input" name="gender" id="gender2" value="P">
+                  							Perempuan
+                						</label>
+              						</div>
+            					</div>
+          				</div>
+        			</div>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Tempat Lahir</label>
+          				<div class="col-sm-10">
+            					<input type="text" class="form-control" name="tempat" id="tempat" placeholder="Tempat Lahir">
+          				</div>
+        			</div>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Tanggal Lahir</label>
+          				<div class="col-sm-5">
+            					<input type="date" class="form-control" name="tgl" id="tgl" placeholder="dd/mm/yyyy"/>
+          				</div>
+        			</div>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Status</label>
+          				<div class="col-sm-10">
+            					<select class="form-control" name="status" id="status">
+              						<option value="admin">Admin</option>
+              						<option value="siswa">Siswa</option>
+            					</select>
+          				</div>
+        			</div>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">File upload</label>
+          				<input type="file" name="foto" id="foto" class="file-upload-default">
+          				<div class="input-group col-sm-10">
+            					<input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+            					<span class="input-group-append">
+              						<button class="file-upload-browse btn btn-gradient-primary" type="button">Upload</button>
+            					</span>
+          				</div>
+        			</div>	
+        			<p class="card-description"> Akun </p>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Username</label>
+          				<div class="col-sm-10">
+            					<div class="input-group mb-2 mr-sm-2">
+              						<div class="input-group-prepend">
+                						<div class="input-group-text">@</div>
+              						</div>
+              						<input type="text" class="form-control" name="username" id="username" placeholder="Username">
+            					</div>
+          				</div>
+        			</div>
+        			<div class="form-group row">
+          				<label class="col-sm-2 col-form-label">Password</label>
+          				<div class="col-sm-10">
+            					<input type="text" class="form-control mb-2 mr-sm-2" name="password" id="password" placeholder="Password">
+          				</div>
+        			</div> 
+        			<button type="submit" class="btn btn-gradient-primary mr-2">Submit</button>
+      			</form>
+    		</div>
+  	</div>
 </div>
+
 <script type="text/javascript">
 	$(document).ready(function()
 	{	$("#formisi").submit(function(e)
@@ -131,7 +132,7 @@
 					title: "Tambah Pengguna Berhasil",
 					message: hasil,
 					callback: function(result){
-					window.location.href = "default.php?kode=tampiluser";
+					window.location.href = "../../?kode=tampiluser";
 				}
 				});
 			})
